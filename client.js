a5f535d37589a219a36db841a7636d3943d8fd2d
BaselMasaoud2 <152072054+BaselMasaoud2@users.noreply.github.com>
2023-12-21 13:24:59 +0200

    Update client.js
    Demo - Exam

--- client.js
+++ client.js
@@ -16,6 +16,19 @@ $(document).ready(function() {
         }
     });
 
+    function getRandomChefName() {
+        return faker.name.firstName();
+    }
+
+    function getRandomRating() {
+        return Math.floor(Math.random() * 5) + 1;
+    }
+
+    function getRatingStars(rating) {
+        const stars = '⭐️'.repeat(rating);
+        return stars;
+    }
+
     function displayRecipes(recipes) {
         let html = '';
         for (let i = 0; i < recipes.length; i++) {
@@ -23,7 +36,11 @@ $(document).ready(function() {
             const title = recipe.title ? recipe.title.trim() : 'No Title';
             const thumbnail = recipe.thumbnail ?? 'https://placekitten.com/300/200';
             const ingredients = recipe.ingredients ? recipe.ingredients.map(ingredient => `* ${ingredient}`).join('<br>') : 'No Ingredients';
-
+            
+            const chefName = getRandomChefName();
+            const rating = getRandomRating();
+            const ratingStars = getRatingStars(rating);
+    
             html += `
                 <div class="recipe">
                     <img src="${thumbnail}" alt="${title}">
@@ -31,20 +48,19 @@ $(document).ready(function() {
                         <div class="recipe-title">${title}</div>
                         <div class="recipe-label">Ingredients:</div>
                         <div class="recipe-ingredients">${ingredients}</div>
+                        <div class="recipe-label">Chef: ${chefName}</div>
+                        <div class="recipe-label">Rating: ${rating}${ratingStars}</div>
+
                     </div>
                 </div>
             `;
         }
-
+    
         $('#recipesContainer').html(html);
     }
-
-
+    
 
     $('#sensitivityFilter').on('change', function () {
         const isChecked = $(this).prop('checked');
     });
 });
-
-
-
