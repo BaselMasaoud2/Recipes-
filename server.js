00b80b71c424a06d9b1514ea970230087a8d5f3a
BaselMasaoud2 <152072054+BaselMasaoud2@users.noreply.github.com>
2023-12-21 13:26:48 +0200

    Update server.js
    Demo Exam

--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
-
 const app = express();
+const faker = require('faker');
 const port = process.env.PORT || 3000;
 const dairyIngredients = ["milk", "cheese", "butter", ];
 const glutenIngredients = ["wheat", "barley", "rye", ];
- 
+//const chefNames = ["basel", "masoud", "ziad", "ahmad", "mhmad", "rami", "razi", "halel"];
+//const chefLastNames = ["Halel","mhamhed","agbaria","akel","helal","hateb","Abo Masoud"];
+
+
 app.use(express.static(__dirname));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -26,13 +29,16 @@ app.get('/style.css', function (req, res) {
 
 const recipeApiUrl = 'https://recipes-goodness-elevation.herokuapp.com/recipes/ingredient';
 
+
 app.get('/recipes', async (req, res) => {
     const ingredient = req.query.ingredient;
     try {
         const response = await axios.get(`${recipeApiUrl}/${encodeURIComponent(ingredient)}`);
         const data = response.data.results || [];
         const filteredData = data.filter(recipe => !containsSensitiveIngredients(recipe.ingredients));
-        res.json(filteredData);
+
+        const chefNamesResponse = { chefNames, chefLastNames };
+        res.json({ recipes: filteredData, chefData: chefNamesResponse });
     } catch (error) {
         console.error(error);
         res.status(500).send(error.message || 'Internal Server Error');
